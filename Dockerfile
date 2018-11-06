6ff8039709a1b749a0bb3a33f67e690a0354d451
Petr Sukhov <petr.sukhov2015@yandex.ru>
2018-11-05 21:06:07 -0800

    test

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-FROM ubuntu:16.04
+FROM inodb/python-selenium
 
 COPY test.py test.py
 COPY parallel_test_run.py parallel_test_run.py
-RUN python parallel_test_run.py
+ENTRYPOINT ["python", "parallel_test_run.py"]
--- test.py
+++ test.py
@@ -3,27 +3,38 @@ import unittest
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import sys
 
-
-class GithubSearchTest(unittest.TestCase):
+class GoogleSearchTest(unittest.TestCase):
 
     def setUp(self):
         #self.browser = webdriver.Chrome()
+	print (sys.argv[0])
 	caps = {'browserName': os.getenv('BROWSER', 'chrome')}
     	self.browser = webdriver.Remote(
-        	command_executor='http://localhost:4444/wd/hub',
+        	command_executor='http://'+sys.argv[1]+':4444/wd/hub',
         	desired_capabilities=caps
     	)
 
 
 
-    def google_search(self):
-	browser = self.browser
-	browser.get("https://google.com/")
-	search_box = driver.find_element_by_name('q')  # Find search input box.
+
+
+
+    def test_github_repo_search(self):
+        browser = self.browser
+        browser.get('https://google.com')
+	search_box = browser.find_element_by_name('q')  # Find search input box.
 	search_box.send_keys('selenium')               # Type in selenium.
 	search_box.send_keys(Keys.RETURN)              # Press ENTER.
-	#self.assertIn('selenium', browser.page_source)
+	time.sleep(10)
+
+        
+
+#search_box = browser.find_element_by_name('q')
+        #search_box.send_keys(Keys.RETURN)
+        #time.sleep(10)  # simulate long running test
+        #self.assertIn('Search more than', browser.page_source)
 
 
 
